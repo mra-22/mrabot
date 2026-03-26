8cd2ac535f9dac55160b8206e52a52ae0a74982d
Arif Rasyid <Ariflgln@gmail.com>
2026-03-26 13:09:24 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
-# Jalankan Python API di background
-python api.py &
+# start.sh
 
-# Jalankan Node WA bot di foreground
+# ---------------- SET LOG ----------------
+LOG_FILE="./api.log"
+echo "===== STARTING BOT & API at $(date) =====" >> $LOG_FILE
+
+# ---------------- START FLASK API ----------------
+# jalankan API di background & redirect stdout/stderr ke log
+python api.py >> $LOG_FILE 2>&1 &
+
+# ---------------- START NODE BOT ----------------
+# jalankan Node WA bot di foreground
 node index.js
