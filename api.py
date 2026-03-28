d1f3b330c0517e5bd351b1155fd54ee5448a5d28
Arif Rasyid <Ariflgln@gmail.com>
2026-03-29 07:47:53 +0800

    Change app port from 3000 to 5000

--- api.py
+++ api.py
@@ -113,4 +113,4 @@ def status():
 
 # ---------------- RUN ----------------
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 3000)))
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
